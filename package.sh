839827468f7e020a6cb92f143f2b0fae68c93a67
ian <ian@ian-desktop.(none)>
2009-05-31 14:48:02 -0400

    added top project directory

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -e "ihx" ];
+then
+    rm -Rf ihx
+    mkdir ihx
+fi
+
+cp -Rupv src/ihx ihx
+cp doc/README ihx
+cp doc/LICENSE ihx
+cp doc/haxelib.xml ihx
+cp bin/ihx.n ihx/run.n
+
+if [ -e "ihx.zip" ];
+then
+    rm ihx.zip
+fi
+zip -r ihx.zip ihx
+
+rm -Rf ihx
