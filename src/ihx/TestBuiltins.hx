67ab1aef1358f418d258a300194abb867a7215eb
ian <ian@ian1.(none)>
2010-03-21 11:14:59 -0400

    updated copyrite

--- src/ihx/TestBuiltins.hx
+++ src/ihx/TestBuiltins.hx
@@ -1,5 +1,5 @@
 /*
- Copyright (c) 2009, Ian Martins (ianxm@jhu.edu)
+ Copyright (c) 2009-2010, Ian Martins (ianxm@jhu.edu)
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
