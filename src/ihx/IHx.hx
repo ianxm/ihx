dc3eb3dde12acbd1842d0c269e9b0a93c0931ead
ian <ianxm@jhu.edu>
2013-11-03 15:15:45 -0500

    bump version

--- src/ihx/IHx.hx
+++ src/ihx/IHx.hx
@@ -27,7 +27,7 @@ import ihx.CmdProcessor;
 **/
 class IHx
 {
-    private static var VERSION = "0.3.1";
+    private static var VERSION = "0.3.2";
 
     /** the source for commands **/
     private var console :ConsoleReader;
