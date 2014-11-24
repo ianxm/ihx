83c76d1f2bf6af928e3cd625d79ab526ccd071b5
Josh Cheek <josh.cheek@gmail.com>
2014-11-23 23:23:18 -0700

    Add functionality to ConsoleReader + small refactorings
    Added Functionality
    -------------------
    
    * ctrlp - scroll back through history (same as up arrow)
    * ctrln - scroll forward through history (same as down arrow)
    * ctrlk - kill to end of line
    * ctrlu - kill to beginning of line
    * ctrly - yank previously killed text
    
    Missing Functionality:
    ----------------------
    
    I have no access to a Windows machine,
    so I can't test what the character codes should be.
    I set them all to -1 to make this really obvious,
    so that anyone who cares will see it and check them.
    
    There is no kill ring or word killing.
    
    Refactorings:
    -------------
    
    Rather than have each command that potentially needs to clear the screen
    have to explicitly do it, we just do it at the end of every
    ConsoleReader#readLine, prior to printing the current command.
    
    This means the `clear` method goes away. We record the command to clear
    before processing the current command (because clearing is done via "\r"
    followed by lots of spaces, so we need to know the old command in order
    to know how many spaces to write). And then we clear the previous
    command just before printing the current command.
    
    This allows the two expression statements like
    `{ clear(cmd); cmd.set(history.prev()); }`
    to be simplified to just `cmd.set(history.prev());` Frankly, I'd do the
    same with the braces on all the control structures, too, but it's not my
    project and some people prefer curly braces like that.
    
    Also, there was some mixed whitespace between spaces and tabs,
    this isn't apparent when your tabstop matches the spaces,
    but I keep mine at 2, so some lines were very out of sync.
    
    And there was some trailing whitespace on some lines.
    I don't really give a shit about things like that,
    but the community seems to be in consensus that trailing whitespace is wrong,
    so my editor just deletes it automatically, and getting it back is difficult.

--- src/ihx/ConsoleReader.hx
+++ src/ihx/ConsoleReader.hx
@@ -34,6 +34,11 @@ typedef CodeSet = {
     var ctrlb     :Int;
     var ctrlf     :Int;
     var ctrld     :Int;
+    var ctrlp     :Int;
+    var ctrln     :Int;
+    var ctrlk     :Int;
+    var ctrlu     :Int;
+    var ctrly     :Int;
 }
 
 /**
@@ -41,7 +46,7 @@ typedef CodeSet = {
 **/
 class ConsoleReader
 {
-    public var cmd(default,null) :PartialCommand;
+    public  var cmd(default,null) :PartialCommand;
     private var code :Int;
     private var history :History;
     private var codeSet :CodeSet;
@@ -61,11 +66,15 @@ class ConsoleReader
         if( std.Sys.systemName() == "Windows" )
             codeSet = {arrow: 224, up: 72, down: 80, right: 77, left: 75, home: 71, end: 79,
                        backspace: 8, ctrlc: 3, enter: 13,
-                       ctrla: 1, ctrle: 5, ctrlb: 2, ctrlf: 6, ctrld: 83 };
+                       ctrla: 1, ctrle: 5, ctrlb: 2, ctrlf: 6, ctrld: 83,
+                       ctrlp: -1, ctrln: -1, ctrlk: -1, ctrlu: -1, ctrly: -1
+            };
         else
             codeSet = {arrow: 27, up: 65, down: 66, right: 67, left: 68, home: 72, end: 70,
                        backspace: 127, ctrlc: 3, enter: 13,
-                       ctrla: 1, ctrle: 5, ctrlb: 2, ctrlf: 6, ctrld: 4 };
+                       ctrla: 1, ctrle: 5, ctrlb: 2, ctrlf: 6, ctrld: 4,
+                       ctrlp: 16, ctrln: 14, ctrlk: 11, ctrlu: 21, ctrly: 25
+            };
     }
 
     // get a command from the console
@@ -74,6 +83,7 @@ class ConsoleReader
         cmd.set("");
         while( true )
         {
+            var clearPrevCommand = cmd.clearString();
             code = Sys.getChar(false);
             // Lib.println("\ngot: " + code +"\n");
             if( code == codeSet.arrow ) // arrow keys
@@ -83,33 +93,38 @@ class ConsoleReader
                 code = Sys.getChar(false);
                 switch( code )
                 {
-                case _ if(code == codeSet.up):    { clear(cmd); cmd.set(history.prev()); }
-                case _ if(code == codeSet.down):  { clear(cmd); cmd.set(history.next()); }
-                case _ if(code == codeSet.right): cmd.cursorForward();
-                case _ if(code == codeSet.left):  cmd.cursorBack();
-                case _ if(code == codeSet.home):  cmd.home();
-                case _ if (code == codeSet.end):   cmd.end();
-				case _ if (code == codeSet.ctrld && std.Sys.systemName() == "Windows"): cmd.del();
+                case _ if(code == codeSet.up ):    cmd.set(history.prev());
+                case _ if(code == codeSet.down):   cmd.set(history.next());
+                case _ if(code == codeSet.right):  cmd.cursorForward();
+                case _ if(code == codeSet.left):   cmd.cursorBack();
+                case _ if(code == codeSet.home):   cmd.home();
+                case _ if(code == codeSet.end):    cmd.end();
+                case _ if(code == codeSet.ctrld && std.Sys.systemName() == "Windows"): cmd.del();
                 }
             }
             else
             {
                 switch( code )
                 {
-                case _ if(code == codeSet.ctrlc): 
+                case _ if(code == codeSet.ctrlc):
                     if( cmd.toString().length > 0 )
                     {
-                        clear(cmd); cmd.set("");
+                        cmd.set("");
                     }
-                    else 
+                    else
                     {
-                        Lib.println(""); 
+                        Lib.println("");
                         std.Sys.exit(1);
                     }
-                case _ if(code == codeSet.enter): 
-                    Lib.println(""); 
-                    history.add(cmd.toString()); 
-                    return cmd.toString(); 
+                case _ if(code == codeSet.enter):
+                    Lib.println("");
+                    history.add(cmd.toString());
+                    return cmd.toString();
+                case _ if(code == codeSet.ctrlp): cmd.set(history.prev());
+                case _ if(code == codeSet.ctrln): cmd.set(history.next());
+                case _ if(code == codeSet.ctrlk): cmd.killRight();
+                case _ if(code == codeSet.ctrlu): cmd.killLeft();
+                case _ if(code == codeSet.ctrly): cmd.yank();
                 case _ if(code == codeSet.ctrld): cmd.del(); // del shares code with tilde?
                 case _ if(code == codeSet.ctrla): cmd.home();
                 case _ if(code == codeSet.ctrle): cmd.end();
@@ -119,13 +134,9 @@ class ConsoleReader
                 case _ if( code>=32 && code<=126 ): cmd.addChar(String.fromCharCode(code));
                 }
             }
+            Lib.print(clearPrevCommand);
             Lib.print(cmd.toConsole());
         }
         return "";
-    }
-
-    public function clear(len)
-    {
-        Lib.print(cmd.clearString());
     }
 }
